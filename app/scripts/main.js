67219a1742312bd944ea1e59a8ce547353ed457c
Renee Vrantsidis <rvrant@gmail.com>
2015-01-21 08:27:19 -0500

    Initial commit of working build. Mostly roughed in, needs some polish.

---
+++ app/scripts/main.js
@@ -0,0 +1,53 @@
+/* jshint devel:true */
+console.log('Curious, eh? My email is renee@renvrant.com, just to remind you.');
+
+$(document).ready(function(){
+
+	function toggleActive(section) {
+		if($(section).offset().top < $(window).scrollTop() + $(window).outerHeight()) {
+			$("nav a").removeClass('active');
+			$("a[href="+section+"]").addClass('active'); 
+		} 
+	}
+
+	$(window).scroll(function() {
+		//Add/remove stickyness
+	    if ($(window).scrollTop() > 100) {
+	        $('.main-nav').addClass('sticky');
+	    } else {
+	        $('.main-nav').removeClass('sticky');
+	    }
+	    toggleActive('#about');
+	    toggleActive('#work');
+	    toggleActive('#contact');
+	    
+
+	});
+
+	$('.mobile-toggle').click(function() {
+	    if ($('.main-nav').hasClass('open-nav')) {
+	        $('.main-nav').removeClass('open-nav');
+	    } else {
+	        $('.main-nav').addClass('open-nav');
+	    }
+	});
+
+	$('.main-nav li a').click(function() {
+	    if ($('.main-nav').hasClass('open-nav')) {
+	        $('.navigation').removeClass('open-nav');
+	        $('.main-nav').removeClass('open-nav');
+	    }
+	});
+
+	// Navigation Scroll - ljepo radi materem
+	$('nav a').click(function(event) {
+	    var id = $(this).attr("href");
+	    var offset = 70;
+	    var target = $(id).offset().top - offset;
+	    $('html, body').animate({
+	        scrollTop: target
+	    }, 500);
+	    event.preventDefault();
+	});
+
+});
